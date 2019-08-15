953c2f56f4672d531c19e1d063ab44b61b882b53
RogerChen98 <chen.rog@husky.neu.edu>
2019-08-15 16:20:09 -0400

    value validator created for cards

--- spec/card_spec.cr
+++ spec/card_spec.cr
@@ -12,4 +12,13 @@ describe Hanabi::Card do
     card.color.should eq(Hanabi::Color::RED)
     card.value.should eq(1)
   end
+
+  it "initializes only with good values" do
+    expect_raises(Exception, "card value must be between 1 and 5 inclusive") do
+      Hanabi::Card.new("red", 6)
+    end
+
+    card = Hanabi::Card.new("red", 5)
+    card.value.should eq(5)
+  end
 end
--- src/card.cr
+++ src/card.cr
@@ -7,11 +7,19 @@ module Hanabi
     def initialize(color : String, value : UInt8)
       @color = Hanabi::Color.of(color)
       @value = value
+
+      validate
     end
 
     def initialize(color : Color, value : UInt8)
       @color = color
       @value = value
+
+      validate
+    end
+
+    def validate
+      raise "card value must be between 1 and 5 inclusive" if @value > 5
     end
   end
 end
