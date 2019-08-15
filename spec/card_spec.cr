78e050f1cc79536a84a861cfec2840ba1e2afe68
RogerChen98 <chen.rog@husky.neu.edu>
2019-08-15 16:37:04 -0400

    spec tests cleaned up by wrapping them into the Hanabi module

--- spec/card_spec.cr
+++ spec/card_spec.cr
@@ -1,26 +1,28 @@
 require "./spec_helper"
 
-describe Hanabi::Card do
-  it "initializes with string" do
-    card = Hanabi::Card.new("red", 1)
-    card.color.should eq(Hanabi::Color::RED)
-    card.value.should eq(1)
-  end
-
-  it "initializes with color" do
-    card = Hanabi::Card.new(Hanabi::Color::RED, 1)
-    card.color.should eq(Hanabi::Color::RED)
-    card.value.should eq(1)
-  end
-
-  it "initializes only with good values" do
-    ([1, 2, 3, 4, 5] of UInt8).each do |value|
-      card = Hanabi::Card.new("red", value)
-      card.value.should eq(value)
+module Hanabi
+  describe Card do
+    it "initializes with string" do
+      card = Card.new("red", 1)
+      card.color.should eq(Color::RED)
+      card.value.should eq(1)
     end
 
-    expect_raises(Exception, "card value must be between 1 and 5 inclusive") do
-      Hanabi::Card.new("red", 6)
+    it "initializes with color" do
+      card = Card.new(Color::RED, 1)
+      card.color.should eq(Color::RED)
+      card.value.should eq(1)
+    end
+
+    it "initializes only with good values" do
+      ([1, 2, 3, 4, 5] of UInt8).each do |value|
+        card = Card.new("red", value)
+        card.value.should eq(value)
+      end
+
+      expect_raises(Exception, "card value must be between 1 and 5 inclusive") do
+        Card.new("red", 6)
+      end
     end
   end
 end
--- spec/color_spec.cr
+++ spec/color_spec.cr
@@ -1,31 +1,33 @@
 require "./spec_helper"
 
-describe Hanabi::Color do
-  it "of Red" do
-    Hanabi::Color.of("red").should eq(Hanabi::Color::RED)
-    Hanabi::Color.of("Red").should eq(Hanabi::Color::RED)
-    Hanabi::Color.of("RED").should eq(Hanabi::Color::RED)
-  end
+module Hanabi
+  describe Color do
+    it "of Red" do
+      Color.of("red").should eq(Color::RED)
+      Color.of("Red").should eq(Color::RED)
+      Color.of("RED").should eq(Color::RED)
+    end
 
-  it "of Green" do
-    Hanabi::Color.of("green").should eq(Hanabi::Color::GREEN)
-  end
+    it "of Green" do
+      Color.of("green").should eq(Color::GREEN)
+    end
 
-  it "of Blue" do
-    Hanabi::Color.of("blue").should eq(Hanabi::Color::BLUE)
-  end
+    it "of Blue" do
+      Color.of("blue").should eq(Color::BLUE)
+    end
 
-  it "of Yellow" do
-    Hanabi::Color.of("yellow").should eq(Hanabi::Color::YELLOW)
-  end
+    it "of Yellow" do
+      Color.of("yellow").should eq(Color::YELLOW)
+    end
 
-  it "of White" do
-    Hanabi::Color.of("white").should eq(Hanabi::Color::WHITE)
-  end
+    it "of White" do
+      Color.of("white").should eq(Color::WHITE)
+    end
 
-  it "of Other" do
-    expect_raises(Exception, "unknown color") do
-      Hanabi::Color.of("gibberish")
+    it "of Other" do
+      expect_raises(Exception, "unknown color") do
+        Color.of("gibberish")
+      end
     end
   end
 end
